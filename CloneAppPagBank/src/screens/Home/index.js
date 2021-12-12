bbb9e579bcef06634dad4909f4dcead64ddd77ee
MarinaPereiraCardozo <cardozo.marina.p@gmail.com>
2021-12-12 17:25:32 -0300

    Commit página home

--- CloneAppPagBank/src/screens/Home/index.js
+++ CloneAppPagBank/src/screens/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TouchableOpacity, View, Image } from 'react-native';
+import { Text, TouchableOpacity, View, Image, TextInput } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { styles } from './style';
 
@@ -38,11 +38,83 @@ export const Home = () => {
                             <Text style={styles.textoSaldo}>Saldo</Text>
                         </View>
                         <View style={styles.containerOlho}>
-                            <Image
-                                style={styles.iconOlho}
-                                source={require('../../assets/olho.png')}
-                            />
+                            <TouchableOpacity>
+                                <Image
+                                    style={styles.iconOlho}
+                                    source={require('../../assets/olho.png')}
+                                />
+                            </TouchableOpacity>
                         </View>
+                    </View>
+                    <View style={styles.containerSaldoValor}>
+                        <Text style={styles.saldo}>R$ 19.789,29</Text>
+                    </View>
+                </View>
+                <View style={styles.containerSaldo}>
+                    <View style={styles.containerTextoValor}>
+                        <Text style={styles.textoValor}>Vendas a receber</Text>
+                    </View>                   
+                    <View style={styles.containerValor}>
+                        <Text style={styles.valor}>R$ 2.744,20</Text>
+                    </View>
+                </View>
+                <TouchableOpacity
+                    style={styles.buttonDetalhes}>
+                    <Text style={styles.buttonDetalhesTexto}>Ver detalhes do saldo</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.containerAzul}>
+                <View style={styles.containerAzulTopo}>
+                    <View style={styles.containerUserIcon}>
+                        <Image
+                            style={styles.userIcon}
+                            source={require('../../assets/user.png')}
+                        />
+                    </View>
+                    <View style={styles.containerTextos}>
+                        <Text style={styles.texto1}>FALTA POUCO!</Text>
+                        <Text style={styles.texto2}>Complete as etapas e libere todos os serviços gratuitos</Text>
+                    </View>
+                    <View style={styles.containerSetinha}>
+                        <TouchableOpacity>
+                            <Image
+                                style={styles.setinha}
+                                source={require('../../assets/seta.png')}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.containerOpcoes1}>
+                    <View style={styles.containerPrincipais}>
+                        <Text style={styles.principais}>Principais</Text>
+                    </View>
+                    <View style={styles.containerProdInvest}>
+                        <Text style={styles.prodInvest}>Produtos e Investimentos</Text>
+                    </View>
+                    <View style={styles.containerServicos}>
+                        <Text style={styles.servicos}>Serviços</Text>
+                    </View>
+                </View>
+                <View style={styles.containerOpcoes2}>
+                    <View style={styles.containerPix}>
+                        <Text style={styles.pix}>Pix/QR Code</Text>
+                    </View>
+                    <View style={styles.containerTransferencias}>
+                        <Text style={styles.transferencias}>Transferências</Text>
+                    </View>
+                    <View style={styles.containerCartoes}>
+                        <Text style={styles.cartoes}>Cartões</Text>
+                    </View>
+                </View>
+                <View style={styles.containerOpcoes3}>
+                    <View style={styles.containerPagar}>
+                        <Text style={styles.pagar}>Pagar Contas</Text>
+                    </View>
+                    <View style={styles.containerRecargas}>
+                        <Text style={styles.recargas}>Recargas e Gift Cards</Text>
+                    </View>
+                    <View style={styles.containerDinheiro}>
+                        <Text style={styles.dinheiro}>Adicionar Dinheiro</Text>
                     </View>
                 </View>
             </View>
--- CloneAppPagBank/src/screens/Home/style.js
+++ CloneAppPagBank/src/screens/Home/style.js
@@ -32,7 +32,9 @@ export const styles = EStyleSheet.create({
     containerSaldo: {
         backgroundColor: '#1C8914',
         padding:10,
-        margin: 10,
+        marginRight: 10,
+        marginLeft: 10,
+        marginBottom: 10,
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
         borderBottomEndRadius: 30,
@@ -40,22 +42,185 @@ export const styles = EStyleSheet.create({
     },
     containerSaldoTopo: {
         flexDirection: 'row',
+        paddingTop: 10,
     },
     containerTextoSaldo: {
-        widht: '50%',
+        widht: '40%',
+        marginLeft: 10,
     },
     containerOlho: {
         widht: '50%',
         alignItems: 'flex-end',
-        marginLeft: 260
+        marginLeft: 240
     },
     textoSaldo:{
         color: 'white',
-        fontSize: 16,
+        fontSize: 18,
     },
     iconOlho: {
         width: 30,
         height: 25,
-        marginLeft: 20,
+        marginLeft: 10,
     },
+    saldo:{
+        color: 'white',
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+    containerSaldoValor: {
+        paddingBottom: 10,
+        marginLeft: 10,
+    },
+    textoValor:{
+        color: 'white',
+        fontSize: 16,
+    },
+    valor:{
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    containerTextoValor: {
+        widht: '40%',
+        marginLeft: 10,
+        marginTop: 5,
+    },
+    containerValor: {
+        paddingBottom: 5,
+        marginLeft: 10,
+    },
+    buttonDetalhes:{
+        backgroundColor: '#186D10',
+        height: 45,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 30,
+        marginBottom: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonDetalhesTexto:{
+        color: '#D4ED8C',
+        fontWeight: 'bold',
+        fontSize:16
+    },
+    containerAzul: {
+        backgroundColor: '#0077AD',
+    },
+    containerAzulTopo: {
+        flexDirection: 'row',
+        justifyContent:'center',
+        padding: 10,
+    },
+    containerTextos:{
+        width: '65%',
+        marginRight: 20,
+        marginLeft: 10,
+    },
+    texto1:{
+        color: 'white',
+        fontSize: 16,
+        marginBottom: 3
+    },
+    texto2:{
+        color: 'white',
+        fontSize: 14,
+    },
+    userIcon: {
+        width: 70,
+        height: 70,
+    },
+    containerSetinha:{
+        justifyContent:'center',
+    },
+    setinha: {
+        width: 20,
+        height: 20,
+    },
+    containerOpcoes1:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 70,
+    },
+    containerPrincipais:{
+        height: '100%',
+        backgroundColor:'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '33%',
+        borderTopLeftRadius: 30,
+    },
+    principais:{
+        fontSize: 16,
+        color: '#306F96',
+        fontWeight: 'bold',
+    },
+    containerProdInvest:{
+        height: '100%',
+        backgroundColor:'#F3F4F6',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '34%',
+    },
+    prodInvest:{
+        fontSize: 16,
+        color: 'black',
+        fontWeight: 'bold',
+        textAlign: 'center' 
+    },
+    containerServicos:{
+        height: '100%',
+        backgroundColor:'#F3F4F6',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '33%',
+        borderTopRightRadius: 30,
+    },
+    servicos:{
+        fontSize: 16,
+        color: 'black',
+        fontWeight: 'bold',
+    },
+    containerOpcoes2:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 120,
+    },
+    containerPix:{
+        height: '100%',
+        backgroundColor:'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '33%',
+    },
+    containerTransferencias:{
+        height: '100%',
+        backgroundColor:'#F3F4F6',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '34%',
+    },
+    containerCartoes:{
+        height: '100%',
+        backgroundColor:'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '33%',
+    },
+    pix:{
+        fontSize: 16,
+        color: 'black',
+    },
+    transferencias:{
+        fontSize: 16,
+        color: '#8C8E8F',
+    },
+    cartoes:{
+        fontSize: 16,
+        color: 'black',
+    },
+
+
 })
